2a8cf0e9e487337cccd1dcf67541a83af3311b0a
awdagency <43855318+awdagency@users.noreply.github.com>
2021-12-27 06:48:12 +0100

    Production Database added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,14 @@
-module.exports = {
-    mongoURI: "mongodb+srv://devlo:1234@cluster0.4rqyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    secret: 'secretkey'
+
+
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        mongoURI: "mongodb+srv://devlo:1234@cluster0.4rqyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        secret: 'secretkey'
+    }
+}
+else{
+    module.exports = {
+        mongoURI: "mongodb+srv://devlo:1234@cluster0.4rqyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        secret: 'secretkey'
+    }
 }
